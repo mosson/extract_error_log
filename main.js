a558f6df2781acd897f3dc194a59d4929272517f
a-munakata <a.munakata@4digit.jp>
2013-05-28 21:34:02 +0900

    argvで入力、エラーの抽出方法を変更
    変更しました。

--- main.js
+++ main.js
@@ -7,8 +7,8 @@
 var getError = require('./modules/getError');
 var concat = require('./modules/concat');
 
-var whichFile = ['production', 'staging'];
-for (var i = 0; i < whichFile.length; i ++) {
-	concat(whichFile[i]);
-	getError(whichFile[i]);
-}
+
+// concat();
+getError();
+
+
--- modules/getError.js
+++ modules/getError.js
@@ -1,26 +1,22 @@
-// GET ERROR LOGS, ADD LINE
-var getError = function(fileName) {
-	
-	// MODULE IMPORT
-	var fs 						= require('fs');
-	var sugar 				= require('sugar');
-	var _ 						= require('../node_modules/underscore');
+#!/usr/bin/env node
 
-	var date 					= Date.create('1 days ago').format('{yyyy}{MM}{dd}');
-	var linePattern 	= /^#{100}[\s\S]*?Completed[\s\S]*?$\n\n\n/gm	;
-	var path 					= __dirname + "/../";		
-	var readFileName 	= path + "/" + date + "/" + fileName + "/export/concat-" + fileName + ".log-" + date;
-	var writeFileName = path + "/" + date + "/" + fileName + "/export/error-" + fileName + ".log-" + date;	
-	var data 					= fs.readFileSync (readFileName, 'ascii');
-	var matchedData 	= data.match(linePattern);
-	
-	_.map(matchedData, function(element) {
+var getError = function() {
+
+	var fs  = require('fs');
+	var _   = require('underscore');
+
+	var ascii_data = fs.readFileSync(process.argv[2], 'ascii');
+	var errors = _.compact( _.map( ascii_data.match(/^Started[\s\S]+?(?=^Started)/mg).slice(1), function(matched){
 		
-		if (element.match(/(Completed[\s][4|5][0-9]{2})/) !== null) {
-			fs.appendFileSync(writeFileName, element);
-		}		
-	});
-}	
+	  if( /Completed\s[45]/.test(matched) ){
+	      return matched;
+	  }else{
+	      return null;
+	  }	
+	}) );
+
+	fs.writeFileSync(process.argv[3], errors, 'ascii');
+
+}
 
 module.exports = getError;
-
