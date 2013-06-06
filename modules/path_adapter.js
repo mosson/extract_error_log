4d4ce842c8a7f8d23a2e2c474a582a022d3fe7bd
a-munakata <a.munakata@4digit.jp>
2013-06-06 15:32:03 +0900

    パスのファクトリを追加

---
+++ modules/path_adapter.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+module.exports 	 = function path_adapter() {	
+	var _ 		  	 = require('underscore'),
+			path       = require('path')
+
+	var path_array 			= [],
+			formated_date		= Date.create().format('{yyyy}{MM}'),
+			base_path 			= path.dirname(__dirname),
+			envs     	 			= ["production", "staging"]
+
+
+	var path_array = _.map(envs, function(env) {		
+		var path_to_date_dir = base_path + "/backup/" + env + "/" + formated_date
+
+		return path_to_date_dir
+	})
+	return path_array	
+}
+
+
