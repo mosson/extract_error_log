520125f3a4d6d8b8ed5b14d7af46e4efca98d4b1
a-munakata <a.munakata@4digit.jp>
2013-06-11 12:24:09 +0900

    secure_copyのファイルのコピー先を修正

--- modules/secure_copy.js
+++ modules/secure_copy.js
@@ -8,28 +8,30 @@ module.exports 			= function secure_copy(path_to_dir) {
 
 	var envs					= ["production", "staging"]
 
-	// CONFIGRATION OF PRODUCTION and STAGING
-	// SEE conf/conf.yml
 
+	_.each([process.env["PATH_TO_LOGS"], process.env["PATH_TO_NGINX_LOGS"]], function(kind_of_logs) {
 
-	var which_conf 	= [
-		conf_production	= {
-			user: process.env["PRODUCTION_USER"],
-			file: process.env["PATH_TO_LOGS"],
-			host: process.env["PATH_TO_PRODUCTION"],
-			path: path_to_dir[0]
-		}, conf_staging = {
-			user: process.env["STAGING_USER"],
-			file: process.env["PATH_TO_NGINX_LOGS"],
-			host: process.env["PATH_TO_STAGING"],
-			path: path_to_dir[1]
-		}]
+		// CONFIGRATION OF PRODUCTION and STAGING
+		// SEE conf/conf.yml
 
-		_.each(which_conf, function(conf) {
-			scp.get(conf, function(err) {
-				if (err) console.log("ERROR! SCP Couldn't finish" + err)
-				else console.log(path_to_dir + " Logs have successfuly transferred.")
+		var which_conf   = [
+			conf_production  = {
+				user: process.env["PRODUCTION_USER"],
+				file: kind_of_logs,
+				host: process.env["PATH_TO_PRODUCTION"],
+				path: path_to_dir[0]
+			}, conf_staging = {
+				user: process.env["STAGING_USER"],
+				file: kind_of_logs,
+				host: process.env["PATH_TO_STAGING"],
+				path: path_to_dir[1]
+			}]
+
+			_.each(which_conf, function(conf) {
+				scp.get(conf, function(err) {
+					if (err) console.log("ERROR! SCP Couldn't finish" + err)
+					else console.log(kind_of_logs + " Logs have successfuly transferred.")
+					})
 				})
 			})
-
 		}
