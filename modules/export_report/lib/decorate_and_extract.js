9f6127a772059fbe24dd618ce0b57c2a10a5bb54
a-munakata <a.munakata@4digit.jp>
2013-06-18 21:17:38 +0900

    テンプレートエンジンで出力

--- modules/export_report/lib/decorate_and_extract.js
+++ modules/export_report/lib/decorate_and_extract.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 module.exports = function decorate_and_extract(data) {
-	var _   = require('underscore');
+	var _   	 = require ('underscore');
+	var ejs 	 = require ('ejs');
+	var fs  	 = require ('fs');
 
 	var errors = _.compact( _.map( data.match(/^Started[\s\S]+?(?=^Started)/mg).slice(1), function(matched){
 
@@ -12,6 +14,10 @@ module.exports = function decorate_and_extract(data) {
 	    }
 	}) );
 	
-  	return errors.join("\n\n##################################################\n\n")
+
+	var data 		= fs.readFileSync('modules/export_report/decorate_data.ejs', 'ascii');
+	var renderd = ejs.render(data, { errors: errors });	
+  	
+	return renderd;
 
 }
