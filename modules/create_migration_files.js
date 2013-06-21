179e1cf53494df193d27fffc4868128a5ffafa8f
a-munakata <a.munakata@4digit.jp>
2013-06-21 13:07:46 +0900

    同じ内容が書き込まれるバグを修正

--- modules/create_migration_files.js
+++ modules/create_migration_files.js
@@ -1,50 +1,54 @@
 #!/usr/bin/env node
 module.exports = function create_migration_files(path_to_dir, env) {
-	var fs  				 = require('fs'),
-	_   				     = require('../node_modules/underscore'),
-	YAML 				     =	require('yamljs'),
-	envs		 		     = ["production", "staging"],
-	ejs	= require('ejs'),
+	var fs  				 = require('fs');
+	var _   				 = require('../node_modules/underscore');
+	var YAML 				 =	require('yamljs');
+	var ejs					 = require('ejs');
     
-	i 					     = 0,
-	test_db_data     = []
+	var i 					 = 0;
+			
+	if (env == "production") {
+		read_dir = path_to_dir[0]
+	} else if (env == "staging") {
+		read_dir = path_to_dir[1]
+	}
 
-	_.each(path_to_dir, function(path_to_date_dir) {
-		var read_dir     = path_to_date_dir		
 
-		fs.readdir(read_dir, function(err, files) {				
-			_.each(files, function(file) {
-				if (file.match(/log/gm)) {
-					var read_file 	 = path_to_date_dir	+ "/" + file
-					var ascii_data   = fs.readFileSync(read_file , 'ascii');	
+	fs.readdir(read_dir, function(err, files) {
+		_.each(files, function(read_file) {
 
-					var entries 	 	 = _.compact( _.map( ascii_data.match(/^Started[\s\S]+?(?=^Started)/mg), function(matched){
-						if( /Completed\s[45]/.test(matched) ){
-							// 正規表現の先読み(?<=pattern)が使えない？
-							test_db_data[0] = matched
-							test_db_data[1] = matched.match(/[0-9]{4}-([0-9]{2}-?){2}\s([0-9]{2}:?){3}\s\+[0-9]{4}/mg)
-							test_db_data[2] = matched.match(/(Completed\s)[0-9]{3}/gm)[0].slice(10)
+			if (read_file.match(/log/gm)) {
+	
+				var ascii_data   = fs.readFileSync(read_dir + "/" + read_file , 'ascii');	
+				var entries 	 	 = _.compact( _.map( ascii_data.match(/^Started[\s\S]+?(?=^Started)/mg), function(matched){
+					if( /Completed\s[45]/.test(matched) ){
+						// 正規表現の先読み(?<=pattern)が使えない？
+						var entry = matched
+						var timestamp = matched.match(/[0-9]{4}-([0-9]{2}-?){2}\s([0-9]{2}:?){3}\s\+[0-9]{4}/mg)
+						var error_status = matched.match(/(Completed\s)[0-9]{3}/gm)[0].slice(10)
 
-							entries = [test_db_data[0], test_db_data[1], test_db_data[2]]
-							return entries
-						} else {
-							return null;
-						}
-					}))
+						entries = [entry, timestamp, error_status]
+						return entries
 
-					data = fs.readFileSync('migration_template.ejs', 'ascii');									
+					} else {
 
-					renderd = ejs.render(data, {entries: entries, env: env});
-					// renderd = renderd.replace(/\n|\r/gim, "\\n");
-					// renderd = renderd.replace(/(\\n)+Log/,"Log");
-					// renderd = renderd.replace(/;(\\n)+/, ";");
+						return null;
 
-					if (read_file.match(/[0-9]{8}/)) {
-						fs.appendFileSync("db/" + env + "-" + read_file.slice(-8) + ".rb", renderd);
-						console.log(renderd);
 					}
-				}				
-			})
+				}))
+
+				data = fs.readFileSync('migration_template.ejs', 'ascii');									
+
+				renderd = ejs.render(data, {entries: entries, env: env});
+				// renderd = renderd.replace(/\n|\r/gim, "\\n");
+				// renderd = renderd.replace(/(\\n)+Log/,"Log");
+				// renderd = renderd.replace(/;(\\n)+/, ";");
+
+				if (read_file.match(/[0-9]{8}/)) {
+					fs.appendFileSync("db/" + env + "-" + read_file.slice(-8) + ".rb", renderd);
+					console.log(renderd);
+				}
+			}				
 		})
-	})
+	})	
 }
