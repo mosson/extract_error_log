b50e0c5783ae747473c95282dc40166e63fa1f95
a-munakata <a.munakata@4digit.jp>
2013-06-25 13:58:54 +0900

    パターンの名前をわかりやすく

--- modules/backup_logs/lib/move_files.js
+++ modules/backup_logs/lib/move_files.js
@@ -1,26 +1,34 @@
 #!/usr/bin/env node
 module.exports 		= function move_files(path_to_dir) {
-	var fs					= require('fs.extra'),
-			_ 					= require('underscore')
+	var fs					= require('fs.extra');
+	var _ 					= require('underscore');
 
-	var envs				= ["production", "staging"],
-			pattern 		= [/(access.log-)|(error.log-)/, /(unicorn.log-)/]
+	var envs						= ["production", "staging"];
+	var pattern_nginx 	= /(access.log-)|(error.log-)/;
+	var pattern_unicorn = /unicorn.log-/;
 
-	_.each(path_to_dir, function(path_to_date_dir) {		
+	_.each(path_to_dir, function(path_to_date_dir) {
+
 		fs.readdir(path_to_date_dir, function(err, files) {			
 			if (err) { console.log (err) }
+
 			_.each(files, function(file) {
-				if (file.match(pattern[0])) {
+
+				if (file.match(pattern_nginx)) {
+					
 					fs.move (path_to_date_dir + "/" + file , path_to_date_dir + "/nginx/" + file , function (err) {
 			    	if (err) { throw err; }
 				    console.log ("Nginx Logs successfully moved: " + path_to_date_dir + "/nginx/" + file);
 					});						
+
 				}
 
-				else if (file.match(pattern[1])) {
+				else if (file.match(pattern_unicorn)) {
+
 					fs.move (path_to_date_dir + "/" + file , path_to_date_dir + "/unicorn/" + file , function (err) {
 			    	if (err) { throw err; }
 				    console.log ("Unicorn Logs successfully moved: " + path_to_date_dir + "/nginx/" + file);
+
 					});
 				}				
 			})					
