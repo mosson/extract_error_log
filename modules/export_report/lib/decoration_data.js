ead6d5312b9310dba61c25cd6d8bcbc1c5f725e4
a-munakata <a.munakata@4digit.jp>
2013-06-25 14:00:47 +0900

    テンプレートを使う

--- modules/export_report/lib/decoration_data.js
+++ modules/export_report/lib/decoration_data.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 module.exports = function decoration_data(data) {	
+	var ejs 	 = require ('ejs');
+	var fs  	 = require ('fs');
 	
 	var _   			= require('underscore');
-	var entry = _.compact( _.map( data.match(/^Started[\s\S]+?(?=^Started)/mg).slice(1), function(matched){			
 
-		return matched;
-    
+	var errors = _.compact( _.map( data.match(/^Started[\s\S]+?(?=^Started)/mg).slice(1), function(matched){
+
+      return matched;
+
 	}) );
-	
-	return entry.join("\n\n##################################################\n\n")
+		
+
+	var data 		= fs.readFileSync('modules/export_report/decorate_data.ejs', 'ascii');
+	var renderd = ejs.render(data, { errors: errors });	
+  	
+	return renderd;
+
 	
 }
-
-
