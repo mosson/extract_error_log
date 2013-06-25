b1144741581a2361abfa12e175999b702ed18955
a-munakata <a.munakata@4digit.jp>
2013-06-25 14:00:19 +0900

    整理

--- modules/path_factory.js
+++ modules/path_factory.js
@@ -1,35 +1,37 @@
 #!/usr/bin/env node
 
 module.exports 	 = function path_factory(usage) {
-	var _ 		  	 = require ('underscore'),
-			path       = require ('path'),
-			sugar			 = require ('sugar'),
-			fs 				 = require ('fs')
+	var _ 		  	 = require ('underscore');
+	var path       = require ('path');
+	var sugar			 = require ('sugar');
+	var fs 				 = require ('fs');
 
-	var path_array 			= [],
-			formated_date		= Date.create().format('{yyyy}{MM}'),
-			base_path 			= path.resolve(__dirname, ".."),
-			envs     	 			= ["production", "staging"]
+	var path_array 			= [];
+	var formated_date		= Date.create().format('{yyyy}{MM}');
+	var base_path 			= path.resolve(__dirname, "..");
+	var envs     	 			= ["production", "staging"];
 
 	var path_to_dir 		= _.map(envs, function(env) {
-	var path_to_backup  = base_path + "/backup/" + env + "/" + formated_date
-	var path_to_report  = base_path + "/report/" + env + "/" + formated_date
-	
-	var arr_backup_files = _.compact(_.map(fs.readdirSync(path_to_backup), function (file) {
-		if (file.match(/(log-[0-9]{8})/)) {
-				return path_to_backup + "/" + file
-			} else {
-				return null
-			}
-		}));
-	var arr_report_files = _.map(arr_backup_files, function(file) {
-		return (path_to_report + "/" + file.slice(-2)) + ".log"
-	})
+		var path_to_backup  = base_path + "/backup/" + env + "/" + formated_date;
+		var path_to_report  = base_path + "/report/" + env + "/" + formated_date;
 		
-	var path_to_files = [arr_backup_files, arr_report_files]
+
+		var arr_backup_files = _.compact(_.map(fs.readdirSync(path_to_backup), function (file) {
+			if (file.match(/(log-[0-9]{8})/)) {
+					return path_to_backup + "/" + file
+				} else {
+					return null
+				}
+		}));
 	
+		var arr_report_files = _.map(arr_backup_files, function(file) {
+			return (path_to_report + "/" + file.slice(-2)) + ".log"
+		})
+			
+		var path_to_files = [arr_backup_files, arr_report_files]
+		
 		switch (usage) {
-			case "backup" :			
+			case "backup" :
 				return path_to_backup
 				break
 			
@@ -43,8 +45,8 @@ module.exports 	 = function path_factory(usage) {
 			
 			default :
 				console.log("Invalid arguments specified.")
-
+			}
 		}
-	})
+	)
 	return path_to_dir
 }
